984c4d62b985975ca674306f404c13e01dcbd8ab
CORP Dbot <crmeade@mtu.edu>
2026-02-13 14:16:12 -0500

    Qualifier command improvements

--- corpoch/dbot/cogs/qualifiercmds.py
+++ corpoch/dbot/cogs/qualifiercmds.py
@@ -21,16 +21,19 @@ class QualifierSelect(discord.ui.Select):
 	async def init(self):
 		qualis = []
 		for qualifier in self.quali.qualifiers:
-			retOpts[qualifier.name] = bracket
-			qualis.append(discord.SelectOption(label=str(qualifier)))
+			theStr = await sync_to_async(lambda : str(qualifier))()
+			self.retOpts[theStr] = qualifier
+			qualis.append(discord.SelectOption(label=theStr))
 		super().__init__(max_values=1, options=qualis, custom_id="bracket_sel")
 
 	async def callback(self, interaction: discord.Integration):
 		qualifier = self.retOpts[self.values[0]]
-		if qualifier.channel != self.quali.ctx.channel.id:
+		if qualifier.channel and qualifier.channel != self.quali.ctx.channel.id:
 			await interaction.respond(f"Please run command in channel (https://discord.com/{self.quali.tourney.guild}/{qualifier.channel}) to submit!", ephemeral=True, delete_after=10)
 		else:
+			await interaction.response.defer(invisible=True)
 			self.quali.qualifier = qualifier
+			await self.quali.show()
 
 class ScreenshotModal(discord.ui.DesignerModal):
 	def __init__(self):
@@ -65,7 +68,6 @@ class DiscordQualifierView(discord.ui.View):
 		self.tourney = None
 		self.steg = None
 		self.screen = None
-		self.doneStartup = False
 
 		cancel = discord.ui.Button(label="Cancel", style=discord.ButtonStyle.red, custom_id="cancelBtn")
 		cancel.callback = self.cancelBtn
@@ -80,24 +82,20 @@ class DiscordQualifierView(discord.ui.View):
 		self.submit.disabled = True
 		self.add_item(self.submit)
 
-	async def show(self):
-		if not self.doneStartup:
-			await self.ctx.defer(ephemeral=True)
-			try:
-				self.tourney = await Tournament.objects.select_related('config').aget(guild=self.ctx.guild.id, active=True)
-			except Tournament.DoesNotExist:
-				await self.ctx.respond("There are no active tournaments running in this server at this time.", ephemeral=True)
-				return
+	async def init(self):
+		await self.ctx.defer(ephemeral=True)
+		try:
+			self.tourney = await Tournament.objects.select_related('config').aget(guild=self.ctx.guild.id, active=True)
+		except Tournament.DoesNotExist:
+			await self.ctx.respond("There are no active tournaments running in this server at this time.", ephemeral=True)
+			return
 
-			if not self.qualifier:
-				async for qualifier in Qualifier.objects.select_related('chart', 'bracket').all().filter(tournament=self.tourney, end_time__gte=timezone.now()):
-					self.qualifiers.append(qualifier)
+		async for qualifier in Qualifier.objects.select_related('chart', 'bracket').all().filter(tournament=self.tourney, end_time__gte=timezone.now()):
+			self.qualifiers.append(qualifier)
 
-			try:
-				self.ply = await TournamentPlayer.objects.aget(user=self.ctx.user.id)
-			except TournamentPlayer.DoesNotExist:
-				self.ply = TournamentPlayer(user=self.ctx.user.id, tournament=self.tourney, ch_name="</Null>")
+		await self.show(init=True)
 
+	async def show(self, init=False):
 		embeds = []
 		if self.qualifier == None and len(self.qualifiers) > 1:
 			qualiSel = QualifierSelect(self)
@@ -111,6 +109,16 @@ class DiscordQualifierView(discord.ui.View):
 				self.qualifier = self.qualifiers[0]
 
 		if self.qualifier:
+			try:
+				self.ply = await TournamentPlayer.objects.aget(user=self.ctx.user.id)
+				self.prev_subs = []
+				async for qual in QualifierSubmission.objects.all().filter(player=self.ply):
+					self.prev_subs.append(qual)
+				self.num_subs = len(self.prev_subs)
+			except TournamentPlayer.DoesNotExist:
+				self.ply = TournamentPlayer(user=self.ctx.user.id, tournament=self.tourney, ch_name="</Null>")
+				self.num_subs
+		
 			embeds.append(self.buildRulesEmbed())
 
 		if self.steg:
@@ -124,8 +132,7 @@ class DiscordQualifierView(discord.ui.View):
 				embeds.append(self.buildSubmitEmbed())
 				self.submit.disabled = False
 			
-		if not self.doneStartup:
-			self.doneStartup = True
+		if init:
 			await self.ctx.respond(embeds=embeds, view=self)
 		else:
 			await self.ctx.edit(embeds=embeds, view=self)
@@ -209,49 +216,25 @@ class DiscordQualifierView(discord.ui.View):
 		embed = discord.Embed(colour=0xFF2800)
 		embed.title = "Qualifier Submission Rules"
 		embed.add_field(name=f"{self.tourney.name} Rules", value=self.tourney.config.rules, inline=False)
+		if self.qualifier.required_submissions > 1:
+			subs_met = '✅' if self.qualifier.required_submissions < self.num_subs else '❌'
+			embed.add_field(name="Required Submissions", value=f"This qualifier requires {self.qualifier.required_submissions} submissions\n\nYou've submitted: {self.num_subs} time(s) {subs_met}")
 		embed.add_field(name=f"Qualifier Rules", value=self.qualifier.rules, inline=False)
-
 		if self.qualifier.form_link and self.qualifier.form_link != "":
 			embed.add_field(name="Qualifier Form Link", value=f"[Link Here]({self.qualifier.form_link})", inline=False)
-
 		embed.add_field(name="Qualifier Chart Link", value=f"[Link Here]({self.qualifier.chart.url})", inline=False)
 		embed.add_field(name="Agreement", value="By submitting a qualifier, you are agreeing to these rules")
-
-
-		#TODO - add 
-
 		return embed
 
 class QualifierCmds(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	qualifier = discord.SlashCommandGroup('qualifier','Clone Hero Tournament Qualifer Commands')
-
-	#TODO: Make a /qualifier path cmd?
-
-	@qualifier.command(name='submit', description='Submit a qualifier score for a tournament this server is running', integration_types={discord.IntegrationType.guild_install})
+	@commands.slash_command(name='qualifier', description='Submit a qualifier score for a tournament/bracket', integration_types={discord.IntegrationType.guild_install})
 	#@discord.option("submission", discord.Attachment, description="Attach in-game screenshot of qualifer run", required=True)
 	async def qualifierSubmitCmd(self, ctx):#, submission: discord.Attachment):
 		view = DiscordQualifierView(ctx)
-		await view.show()
-	
-	@qualifier.command(name='status', description='Shows the status of your qualifier for an active tournament', integration_types={discord.IntegrationType.guild_install})
-	async def qualifierSubmitCmd(self, ctx):
-		view = DiscordQualifierView(ctx)
 		await view.init()
-
-	@qualifier.command(name='info', description='Shows the info for an active tournament qualifier', integration_types={discord.IntegrationType.guild_install})
-	async def qualifierSubmitCmd(self, ctx):
-		view = DiscordQualifierView(ctx)
-		await view.init()
-
-	@commands.Cog.listener()
-	async def on_application_command_error(self, ctx: discord.ApplicationContext, error: discord.DiscordException):
-		if isinstance(error, commands.NotOwner):
-			await ctx.respond("You don't own me! (You cannot run this command)", ephemeral=True)
-		else:
-			raise error
 
 def setup(bot):
 	bot.add_cog(QualifierCmds(bot))
