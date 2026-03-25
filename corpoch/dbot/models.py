951939009407ffef18d2fb7e58cca94509b9ea1c
CORP Dbot <crmeade@mtu.edu>
2026-03-25 15:37:22 -0400

    Corpoch Task to update guilds

--- corpoch/dbot/models.py
+++ corpoch/dbot/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from corpoch.models import CHIcon
 #from corpoch.db.tasks
 
 class CHEmoji(models.Model):
 	id = models.BigIntegerField(verbose_name="AppEmoji ID", db_index=True, primary_key=True)
-	icon = models.ForeignKey(CHIcon, related_name="discord", verbose_name="Emote ID", null=False, blank=False, default=-1, on_delete=models.CASCADE)
+	icon = models.ForeignKey('corpoch.CHIcon', related_name="discord", verbose_name="Emote ID", null=False, blank=False, default=-1, on_delete=models.CASCADE)
 
 	class Meta:
 		verbose_name = "Chart Icon"
