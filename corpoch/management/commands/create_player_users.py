41958d20d43f2858e0b538ddb89cfe8a3c8c0da1
CORP Dbot <crmeade@mtu.edu>
2026-03-27 14:33:48 -0400

    Create Users from Players mgmt cmd

---
+++ corpoch/management/commands/create_player_users.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from corpoch.models import DiscordUser, TournamentPlayer, Tournament
+from DiscordOauth2 import settings
+import corpoch.dbot.tasks 
+
+class Command(BaseCommand):
+	help = 'Create DiscordUsers from TournamentPlayers'
+
+	def add_arguments(self, parser):
+		parser.add_argument('-d', '--discord-id', type=int, help='Discord Guild ID of user to make Users from TournamentPlayers for')
+
+	def handle(self, *args, **options):
+		gid = options['discord_id']
+		if gid:
+			tourney = Tournament.objects.get(active=True, guild__id=gid)
+			objs = TournamentPlayer.objects.all().filter(tournament=tourney, is_active=True)
+		else:
+			objs = TournamentPlayer.objects.all()
+
+		for ply in objs:
+			try:
+				user = DiscordUser.objects.get(id=ply.user)
+			except DiscordUser.DoesNotExist:
+				user = DiscordUser(id=ply.user)
+				print(f"Creating User {ply.user} - {ply.name} + sending task to update info from discord")
+				user.save()
+				corpoch.dbot.tasks.update_user(user.id)
