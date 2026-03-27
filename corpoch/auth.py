7ed33510c28b9f999e914ac64b73da874464b4fe
CORP Dbot <crmeade@mtu.edu>
2026-03-27 13:04:50 -0400

    Reorg user update

--- corpoch/auth.py
+++ corpoch/auth.py
@@ -7,20 +7,20 @@ class DiscordBackend(ModelBackend):
 		UserModel = get_user_model()
 		try:
 			check_user = UserModel.objects.get(id=user.id)
-		except:
-			new_user = UserModel.objects.create_new_discord_user(user)
-			return new_user
-
-		check_user.username = user.global_name if user.global_name else user.display_name
-		check_user.global_name = user.global_name if user.global_name else user.display_name
-		check_user.avatar = user.avatar
-		check_user.public_flags = user.public_flags
-		check_user.flags = user.flags
-		check_user.locale = user.locale
-		check_user.mfa_enabled = user.mfa_enabled
-		check_user.last_login = timezone.now()
-		check_user.save()
-		return check_user
+		except UserModel.DoesNotExist:
+			check_user = UserModel.objects.create_new_discord_user(user)
+		else:
+			check_user.username = user.global_name if user.global_name else user.display_name
+			check_user.global_name = user.global_name if user.global_name else user.display_name
+			check_user.avatar = user.avatar
+			check_user.public_flags = user.public_flags
+			check_user.flags = user.flags
+			check_user.locale = user.locale
+			check_user.mfa_enabled = user.mfa_enabled
+			check_user.last_login = timezone.now()
+			check_user.save()
+		finally:
+			return check_user
 
 	def get_user(self, user_id):
 		UserModel = get_user_model()
