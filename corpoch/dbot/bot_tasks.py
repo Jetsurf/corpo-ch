854f2ff5be12f46acd3bece48a3e5d5cb3804272
Jetsurf <crmeade@mtu.edu>
2026-04-21 18:59:09 -0400

    CHOpt+Bugfixes
    * Clamp failure on discorduser update
    
    * CHOpt 1.13.0
    
    * Write out server display name to tournamentplayer

--- corpoch/dbot/bot_tasks.py
+++ corpoch/dbot/bot_tasks.py
@@ -166,10 +166,14 @@ async def update_user(bot, user_id):
 	dbuser.username = duser.global_name if duser.global_name else duser.display_name
 	dbuser.global_name = duser.global_name if duser.global_name else duser.display_name
 	dbuser.avatar = duser.display_avatar.url
-	for ply in dbuser.tournaments.all().filter(is_active=True):
+	for ply in dbuser.tournaments.all():
 		guild = bot.get_guild(ply.tournament.guild.id)
 		if guild:
-			member = await guild.fetch_member(dbuser.id)
-			ply.name = member.global_name if member.global_name else member.display_name
-
+			try:
+				member = await guild.fetch_member(dbuser.id)
+			except discord.NotFound:
+				continue
+			else:
+				ply.name = member.display_name if member.display_name else member.global_name
+				await ply.asave()
 	await dbuser.asave()
