6932febe032e2c5fec865a3be65ffdba2540dfe3
CORP Dbot <crmeade@mtu.edu>
2026-05-04 17:46:38 -0400

    Heavily reworked DiscordAuth/Tokens workflow

--- corpoch/models.py
+++ corpoch/models.py
@@ -1,4 +1,5 @@
-import uuid, typing, json, math, io, pydantic
+import uuid, typing, json, math, io, pydantic, datetime
+from requests import Session
 
 from django.contrib import admin
 from django.contrib.auth.models import AbstractUser
@@ -64,12 +65,76 @@ class DiscordUser(AbstractUser):
 			return str(self.id)
 
 class DiscordToken(models.Model):
+	id = models.AutoField(primary_key=True, help_text="Internal ID of a token.")
 	access_token = EncryptedTextField(max_length=255)
 	refresh_token = EncryptedTextField(max_length=255)
-	user = models.OneToOneField("DiscordUser", null=True, on_delete=models.CASCADE, related_name="token")
+	scopes = EncryptedTextField(max_length=64, default='identify guilds')
+	user = models.OneToOneField(DiscordUser, null=True, on_delete=models.CASCADE, related_name="token")
+	expires = models.DateTimeField(verbose_name="Expiry Time", default=timezone.now, help_text="Token expiry time.")
 	#This needs an expiry date field to trigger refreshes - refresh tokens need to be handled
-	def __str__(self):
-		return f"self.discord_user.global_name"
+
+	class AuthError(Exception):
+		def __init__(self, msg) -> None:
+			super().__init__(msg)
+
+	@property
+	def __auth_header(self):
+		return (settings.BOT_ID, settings.BOT_SECRET)
+
+	@property
+	def __oauth_header(self):
+		return {'Authorization': f'Bearer {self.access_token}'}
+
+	def login(self, code=None) -> None:
+		self.__session = Session()
+		self.__base_url = "https://discord.com/api/v10"
+		self.__content_header = {'Content-Type': 'application/x-www-form-urlencoded'}
+		self.__auth = { "client_id": settings.BOT_ID, "client_secret": settings.BOT_SECRET }
+		if not code:
+			if not self.access_token or not self.refresh_token:
+				raise AuthError("Access/Refresh token not set and code is none!")
+			else:
+				self.__data = { "grant_type": "refresh_token", "refresh_token": self.refresh_token }
+		else:
+			self.__data = { "grant_type": "authorization_code",	"code": code, "redirect_uri": settings.REDIRECT_URI }
+			self.__exchange_code()
+	
+	def __exchange_code(self):
+		response = self.__session.post(f"{self.__base_url}/oauth2/token", data=self.__data, headers=self.__content_header, auth=self.__auth_header)
+		if response.status_code == 200:
+			self.__update(response.json())
+			if self.id:
+				self.save()
+			return
+		raise self.AuthError(f"Failed to connect to discord API {response.json()}")
+	
+	def update_code(self) -> str:
+		if self.expires < timezone.now() + datetime.timedelta(days=2):
+			self.__exchange_code()
+
+	def __update(self, json: dict):
+		self.access_token = json["access_token"]
+		self.refresh_token = json["refresh_token"]
+		self.scopes = json['scope']
+		self.expires = timezone.now() + datetime.timedelta(seconds=json['expires_in'])
+
+	def identity(self) -> dict:
+		if not self.access_token:
+			self.__exchange_code()
+
+		response =  self.__session.get(f"{self.__base_url}/users/@me", headers=self.__oauth_header)
+		if response.status_code == 200:
+			return response.json()
+		raise AuthError("Failed to connect to discord API")
+
+	def guilds(self) -> list:
+		if not self.access_token:
+			self.__exchange_code()
+
+		response = self.__session.get(f"{self.__base_url}/users/@me/guilds", headers=self.__oauth_header)
+		if response.status_code == 200:
+			return response.json()
+		raise AuthError("Failed to connect to discord API")
 
 class CHIcon(models.Model):
 	"""
